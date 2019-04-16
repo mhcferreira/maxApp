6aa9f6d83fb30333b049b474748d226b6e8976f8
mhcferreira <matheushenriquecoelhoferreira@gmail.com>
2019-04-16 15:19:13 -0300

    versao 16/04

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+val1:Number = 0;
+val2:Number = 0;
 
+somar() :void{
+  let variavel1  =Number(this.val1);
+  let variavel2 = Number(this.val2);
+  let somar = (variavel1+variavel2);
+   alert("o resultado é " + somar);
+   if(somar % 2 ==0){
+     alert("é par");
+   }else{
+     alert("é impar");
+   }
+}
 }
